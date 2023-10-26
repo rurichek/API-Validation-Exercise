f77831b94daebba9cc142c3eb91a0e2ed6af7985
Timour <rurichek@hotmail.com>
2023-10-26 13:55:57 -0400

    first commit

---
+++ routes/books.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const Book = require("../models/book");
+const { validate } = require("jsonschema");
+const bookSchema = require("../schemas/book_schema.json");
+
+
+
+const router = new express.Router();
+
+
+/** GET / => {books: [book, ...]}  */
+
+router.get("/", async function (req, res, next) {
+  try {
+    const books = await Book.findAll(req.query);
+    return res.json({ books });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[id]  => {book: book} */
+
+router.get("/:id", async function (req, res, next) {
+  try {
+    const book = await Book.findOne(req.params.id);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST /   bookData => {book: newBook}  */
+
+router.post("/", async function (req, res, next) {
+  console.log(req.body);
+  try {
+    // Validate the incoming data with the JSON schema
+    const result = validate(req.body, bookSchema);
+    if (!result.valid) {
+      // If validation fails, return a 400 error with the validation issues
+      return res.status(400).json({ errors: result.errors.map(e => e.stack) });
+    }
+    const book = await Book.create(req.body);
+    return res.status(201).json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PUT /[isbn]   bookData => {book: updatedBook}  */
+
+router.put("/:isbn", async function (req, res, next) {
+  try {
+    const book = await Book.update(req.params.isbn, req.body);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[isbn]   => {message: "Book deleted"} */
+
+router.delete("/:isbn", async function (req, res, next) {
+  try {
+    await Book.remove(req.params.isbn);
+    return res.json({ message: "Book deleted" });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
